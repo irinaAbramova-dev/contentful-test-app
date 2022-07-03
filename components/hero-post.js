88d68efbad76ff58cae70462394ac32fe795a33c
irinaP-local <iryna.abramava@gmail.com>
2022-07-03 22:24:23 +0300

    logs

--- components/hero-post.js
+++ components/hero-post.js
@@ -13,9 +13,9 @@ export default function HeroPost({
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
-      </div>
+      {/*<div className="mb-8 md:mb-16">*/}
+      {/*  <CoverImage title={title} slug={slug} url={coverImage.url} />*/}
+      {/*</div>*/}
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
